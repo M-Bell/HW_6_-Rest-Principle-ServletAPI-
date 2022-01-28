672718052ba3413950ed01f424fcef1276ffc5fb
Maksym Shyiko <maxymshyjko@gmail.com>
2022-01-28 21:10:10 +0200

    HW_6

--- src/main/java/com/example/restservice/controller/UserController.java
+++ src/main/java/com/example/restservice/controller/UserController.java
@@ -2,13 +2,7 @@ package com.example.restservice.controller;
 
 import com.example.restservice.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -29,16 +23,15 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
         Optional<User> userOptional = users.stream().filter(user -> user.getId() == id).findFirst();
 
-        if (userOptional.isPresent()) {
-            return ResponseEntity.ok(userOptional.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return userOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> addUser(@RequestBody User user) {
         if (users.stream().anyMatch(user1 -> user1.getId() == user.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (users.stream().anyMatch(user1 -> user1.getSurname().equals(user.getSurname()))) {
             return ResponseEntity.badRequest().build();
         }
         users.add(user);
@@ -47,8 +40,14 @@ public class UserController {
 
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@RequestBody User userBody, @PathVariable("id") int id) {
+        if (users.stream()
+                .anyMatch(user1 -> user1.getId() != userBody.getId()
+                        && user1.getSurname().equals(userBody.getSurname()))) {
+
+            return ResponseEntity.badRequest().build();
+        }
         for (User user : users) {
-            if(user.getId() == id) {
+            if (user.getId() == id) {
                 user.setAge(userBody.getAge());
                 user.setName(userBody.getName());
                 user.setSurname(userBody.getSurname());
